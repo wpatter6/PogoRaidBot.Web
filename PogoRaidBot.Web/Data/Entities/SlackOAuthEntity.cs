7897a325acf9426a2c47b8f7330bfc23e01f3e92
Will Patterson <wpatter6@gmail.com>
2017-09-22 01:19:20 -0600

    Starting EF setup

---
+++ PogoRaidBot.Web/Data/Entities/SlackOAuthEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogoRaidBot.Web.Data.Entities
+{
+    public class OAuthCodeEntity
+    {
+        public ulong Id { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ PogoRaidBot.Web/Data/PogoRaidBotWebDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PogoRaidBot.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogoRaidBot.Web.Data
+{
+    public class PogoRaidBotWebDbContext : DbContext
+    {
+        private string connectionString;
+
+        public PogoRaidBotWebDbContext(string cstr)
+        {
+            connectionString = cstr;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<OAuthCodeEntity>().HasKey(c => c.Id);
+        }
+
+        public DbSet<OAuthCodeEntity> OAuthCodes { get; set; }
+    }
+}
